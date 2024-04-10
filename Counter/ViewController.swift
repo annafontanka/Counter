9924ce5e0785f0e9facd99a2b81c55ec46ec0d4f
Anna.V <anna.alekhina.online@gmail.com>
2024-04-10 17:17:22 +0300

    Done

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,26 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Anna on 10.04.2024.
+//  Created by Anna on 02.04.2024.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    private  var startcount:Int = 0
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    @IBAction func didTapButton(_ sender: Any) {
+        if startcount >= 0 {
+            startcount += 1
+            print (startcount)
+            myLabel.text = "Значение счетчика: " + String(startcount)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
 
